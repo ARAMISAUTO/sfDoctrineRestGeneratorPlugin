567856531cff68a08a73adedd7caed5496e36fb0
Stéphane Lebréquier <stephane.lebrequier@aramisauto.com>
2011-12-07 18:09:18 +0100

    JSON objects are now camelize as the XML ones already were.

--- lib/serializer/sfResourceSerializerJson.class.php
+++ lib/serializer/sfResourceSerializerJson.class.php
@@ -9,11 +9,29 @@ class sfResourceSerializerJson extends sfResourceSerializer
 
   public function serialize($array, $rootNodeName = 'data', $collection = true)
   {
+    $array = $this->camelizeArray($array);
     return json_encode($array);
   }
 
   public function unserialize($payload)
   {
     return json_decode($payload, true);
+  }
+
+  protected function camelizeArray($array)
+  {
+    foreach ($array as $key => $value)
+    {
+      unset($array[$key]);
+      $key = $this->camelize($key);
+
+      if (is_array($value)) {
+        $array[$key] = camelize($value);
+      } else {
+        $array[$key] = $value;
+      }
+    }
+
+    return $array;
   }
 }

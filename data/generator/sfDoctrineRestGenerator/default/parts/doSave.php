e7ef193e1c0dca2f1a0226b936d9668953a09619
Stéphane Lebréquier <stephane.lebrequier@aramisauto.com>
2011-12-13 16:20:18 +0100

    [listener] Unhardcode moduleName + adding module name to event trigger

--- data/generator/sfDoctrineRestGenerator/default/parts/doSave.php
+++ data/generator/sfDoctrineRestGenerator/default/parts/doSave.php
@@ -2,12 +2,20 @@
   {
     $this->object->save();
 
-    $this->dispatcher->notify(new sfEvent($this->object, 'sfDoctrineRestGenerator.save.post'));
+    if ($this->dispatcher->hasListeners('sfDoctrineRestGenerator.save.post'))
+    {
+      $this->object = $this->dispatcher->notify(
+        new sfEvent(
+          $this->object,
+          'sfDoctrineRestGenerator.<?php echo $this->getModuleName(); ?>.save.post'
+        )
+      );
+    }
 
     // Set a Location header with the path to the new / updated object
     $this->getResponse()->setHttpHeader('Location', $this->getController()->genUrl(
       array_merge(array(
-        'sf_route' => 'asRestProspect_show',
+        'sf_route' => '<?php echo $this->getModuleName(); ?>_show',
         'sf_format' => $this->getFormat(),
       ), $this->object->identifier())
     ));

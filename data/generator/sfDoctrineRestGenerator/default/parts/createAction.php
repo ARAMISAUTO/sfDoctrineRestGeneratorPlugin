9d75a97216e1faeda5e5111618ffe20aab19e594
Stéphane Lebréquier <stephane.lebrequier@aramisauto.com>
2012-10-03 18:02:08 +0200

    [fix] Makes executeUpdate returns 200 and empty body instead of nothing

--- data/generator/sfDoctrineRestGenerator/default/parts/createAction.php
+++ data/generator/sfDoctrineRestGenerator/default/parts/createAction.php
@@ -28,6 +28,14 @@
       $this->updateObjectFromRequest($content);
       $this->doSave();
 
+      // Set a Location header with the path to the new / updated object
+      $this->getResponse()->setHttpHeader('Location', $this->getController()->genUrl(
+        array_merge(array(
+          'sf_route' => '<?php echo $this->getModuleName(); ?>_show',
+          'sf_format' => $this->getFormat(),
+        ), $this->object->identifier())
+      ));
+
       return sfView::HEADER_ONLY;
     }
     catch (Exception $e)
--- data/generator/sfDoctrineRestGenerator/default/parts/doSave.php
+++ data/generator/sfDoctrineRestGenerator/default/parts/doSave.php
@@ -8,12 +8,4 @@
         'sfDoctrineRestGenerator.<?php echo $this->getModuleName(); ?>.save.post'
       )
     );
-
-    // Set a Location header with the path to the new / updated object
-    $this->getResponse()->setHttpHeader('Location', $this->getController()->genUrl(
-      array_merge(array(
-        'sf_route' => '<?php echo $this->getModuleName(); ?>_show',
-        'sf_format' => $this->getFormat(),
-      ), $this->object->identifier())
-    ));
   }
